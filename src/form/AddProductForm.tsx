026ffab889d880e0f70a77e5485dc47aee428a43
yakintech <cagatayyildiz87@gmail.com>
2023-10-31 14:22:56 +0300

    yup sample

--- src/form/AddProductForm.tsx
+++ src/form/AddProductForm.tsx
@@ -1,6 +1,8 @@
 import axios from 'axios'
 import React from 'react'
 import { SubmitHandler, useForm } from 'react-hook-form'
+import { yupResolver } from "@hookform/resolvers/yup"
+import * as yup from "yup"
 
 type Inputs = {
     name: string
@@ -10,17 +12,25 @@ type Inputs = {
 }
 
 
+const addProductShema = yup.object().shape({
+    name: yup.string().required("Name alanı boş bırakılamaz!"),
+    unitPrice: yup.number().required("unitPrice alanı boş bırakılamaz!")
+
+})
+
 
 function AddProductForm() {
 
 
     //register ile inputları eşleştireceğim
-    const { register, handleSubmit } = useForm<Inputs>()
+    const { register, handleSubmit, formState: { errors }, } = useForm<any>({
+        resolver: yupResolver(addProductShema)
+    })
 
-    const addProduct: SubmitHandler<Inputs> = (data : Inputs) => {
+    const addProduct: SubmitHandler<Inputs> = (data: Inputs) => {
 
         console.log(data);
-        
+
         // axios.post('https://northwind.vercel.app/api/products', data)
         //     .then(res => {
         //         console.log(res);
@@ -35,10 +45,13 @@ function AddProductForm() {
                 <div>
                     <label htmlFor="">Name</label>
                     <input type='text' {...register("name")} />
+                    { errors.name && <span style={{color:'red'}}>{errors.name.message?.toString()}</span>}
                 </div>
                 <div>
                     <label htmlFor="">Unit Price</label>
                     <input {...register('unitPrice')} />
+                    { errors.unitPrice && <span style={{color:'red'}}>{errors.unitPrice.message?.toString()}</span>}
+
                 </div>
                 <div>
                     <label htmlFor="">Stock</label>
